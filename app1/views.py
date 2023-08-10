969fb0a888f3770ba0f3e78ce4eda11701c58645
simhadrigithub <murugeshsimhadri88888@gmail.com>
2023-08-10 19:11:51 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def cdnmethod(request):
+    return render(request,'cdnmethod.html')
+def cdnmethod5(request):
+    return render(request,'cdnmethod5.html')
